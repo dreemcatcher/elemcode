128f52c7f6a966f167684586a078415f5c945297
Anton Vinogradov <etheralgreen@gmail.com>
2016-10-04 23:46:25 +1100

    changes in code positioning

--- tasks/2/index.php
+++ tasks/2/index.php
@@ -5,8 +5,5 @@ $dollars = 200; /* Число долларов */
 $exchangeRate = 32.24; /* Курс обмена */
 $roubles = $dollars*$exchangeRate;  /* А вот эту строчку надо дописать самому */
 
-
-
 echo "$dollars долларов можно обменять на $roubles рублей";
-
 ?>
