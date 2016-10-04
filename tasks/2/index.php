b5f217834f84f1c16f625bb5bd69110bec5d3c12
Anton Vinogradov <etheralgreen@gmail.com>
2016-10-04 23:33:46 +1100

    w2 resolved.

--- tasks/2/index.php
+++ tasks/2/index.php
@@ -1,4 +1,12 @@
 <?php
 error_reporting(-1);
-echo "Hello World";
+
+$dollars = 200; /* Число долларов */
+$exchangeRate = 32.24; /* Курс обмена */
+$roubles = $dollars*$exchangeRate;  /* А вот эту строчку надо дописать самому */
+
+
+
+echo "$dollars долларов можно обменять на $roubles рублей";
+
 ?>
