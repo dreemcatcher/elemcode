58e847b693da98bbbdfb89a47d8cccabe2672159
Anton Vinogradov <etheralgreen@gmail.com>
2016-10-07 17:50:41 +1100

    Task 6 array resolved task 7 array started

--- tasks/7/index.php
+++ tasks/7/index.php
@@ -1,7 +1,15 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: O
- * Date: 10/7/2016
- * Time: 5:41 PM
- */
+
+error_reporting(-1);
+
+/* Оценки, которые поставили фотке анона в одноклассниках */
+$rates = array(3, 5, 3, 2, 1, 8, 4, 3, 4, 3, 2, 3);
+
+/* Посчитаем среднюю оценку. Для этого найдем сумму всех оценок, и поделим
+ее на их количество */
+$ratesSum = array_sum($rates);
+$ratesCount = count($rates); /* Количество оценок — эту строчку надо дописать самому */
+$averageRate = $ratesSum/$ratesCount; /* Средняя оценка — допиши сам */
+
+echo "Анону поставили {$ratesCount} оценок, их сумма = {$ratesSum}\nСредний балл — {$averageRate}\n";
+?>
