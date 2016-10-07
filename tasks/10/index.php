d95e4d23ccffbf1217f526aeb1fe449b6452b166
Anton Vinogradov <etheralgreen@gmail.com>
2016-10-07 20:36:27 +1100

    Namegenerator solved

--- tasks/10/index.php
+++ tasks/10/index.php
@@ -1,7 +1,24 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: o
- * Date: 07.10.2016
- * Time: 19:56
- */
+error_reporting(-1);
+/* Слоги, из которых составляется имя */
+$letters = array(
+    'ко',   'и',    'дзу',  'ми',
+    'са',   'ку',   'ра',   'да',
+    'чи',   'а',    'ки',   'ми',
+    'на',   'го',   'ха',   'ру'
+);
+/* В эту переменную запишем получившееся имя */
+$name = ' ';
+/* Гененрируем 4 слога */
+for ($i = 1; $i <= 4; $i++) {
+    /* Выкидываем случайное число (count - число элементов в массиве) */
+    $random = array_rand($letters);
+    $randomText = $letters[$random];
+//    echo "<pre>";
+//    print_r ($letters);
+//    echo "</pre>";
+    echo "Выпало число {$random}, слог {$randomText}\n";
+    $name=$randomText . $name;
+}
+echo "------\n";
+echo "Советую имя: {$name} - не прогадаешь!\n";
