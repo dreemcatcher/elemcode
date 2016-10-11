fd83c6f37322b1c5c295376cb478dd856c47812b
Anton Vinogradov <etheralgreen@gmail.com>
2016-10-11 15:51:04 +1100

    Ipadcredit tast resolved

--- tasks/15/index.php
+++ tasks/15/index.php
@@ -1,7 +1,33 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: O
- * Date: 10/11/2016
- * Time: 10:52 AM
- */
+Function kredit($credit,$percent,$comission,$openDeposit)
+{
+    $potracheno = 0;
+    $dolg = $credit + $openDeposit;
+    for ($i = 0; $i < 50; $i++) {
+        $dolg = $dolg + $comission + ($dolg * ($percent / 100));
+        if ($dolg > 5000) {
+            $dolg = $dolg - 5000;
+            $potracheno = $potracheno + 5000;
+            //echo $i . " месяц. Долг равен " . $dolg . ". Выплачиваем 5000<br>";
+        } elseif ($dolg < 5000) {
+            //echo $i . " месяц. Долг равен " . $dolg . ". Выплачиваем {$dolg}. Поздравляем с последней выплатой.<br>";
+            $dolg= $dolg - $dolg;
+            $potracheno = $potracheno + $dolg;
+            break;
+        } elseif ($dolg < 0) {
+            //echo "Что-то пошло не так.";
+            break;
+        }
+    }
+    //echo $potracheno . " потрачено<br>";
+    //echo $i . "месяцев<br>";
+    return $potracheno;
+}
+
+$homoCreditTotal =  kredit(39999, 4, 500, 0);
+$softBankTotal =  kredit(39999, 3, 1000, 0);
+$strawberryBankTotal =  kredit(39999, 2, 0, 7777);
+
+echo "homoCredit: {$homoCreditTotal} руб. \n";
+echo "softBank: {$softBankTotal} руб. \n";
+echo "strawberryBank: {$strawberryBankTotal} руб. \n";
