ba9e5a53b24ac43c53691308d190658d31032f8e
Anton Vinogradov <etheralgreen@gmail.com>
2016-10-27 12:09:41 +1100

    Schitalochka solved

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 <div align="center">
     <h1>Домашние работы отсюда http://archive-ipq-co.narod.ru/</h1>
 
-    <?php for($i=1; $i<30; $i++){?>
+    <?php for($i=1; $i<32; $i++){?>
         <a href="tasks\<?php echo $i?>\index.php"><?php echo $i?></a><br>
     <?php } ?>
 
