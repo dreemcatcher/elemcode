163f2b377cbe311f33954efe69b96922ae17c426
Anton Vinogradov <etheralgreen@gmail.com>
2016-11-07 17:32:34 +1100

    all code upgraded.

--- tasks/15/index.php
+++ tasks/15/index.php
@@ -1,34 +1,37 @@
 <?php
-Function kredit($credit,$percent,$comission,$openDeposit)
+header("Content-Type: text/plain; charset=utf-8");
+function kredit($credit, $percent, $comission, $openDeposit)
 {
-    $potracheno = 0;
-    $dolg = $credit + $openDeposit;
+    $spent = 0;
+
+    $debt = $credit + $openDeposit;
     for ($i = 0; $i < 50; $i++) {
-        $dolg = $dolg + $comission + ($dolg * ($percent / 100));
-        if ($dolg > 5000) {
-            $dolg = $dolg - 5000;
-            $potracheno = $potracheno + 5000;
-            echo $i . " месяц. Долг равен " . $dolg . ". Выплачиваем<br>";
-            echo " Выплата по долгу равна " . $potracheno . ". Выплачиваем 5000<br>";
-        } elseif ($dolg < 5000) {
-            echo $i . " месяц. Долг равен " . $dolg . ". Выплачиваем {$dolg}. Поздравляем с последней выплатой.<br>";
-            $potracheno = $potracheno + $dolg;
-            $dolg= $dolg - $dolg;
-            echo " Выплата по долгу равна " . $potracheno . ". Выплачиваем 5000<br>";
+        $debt = $debt + $comission + ($debt * ($percent / 100));
+        if ($debt > 5000) {
+            $debt = $debt - 5000;
+            $spent = $spent + 5000;
+            echo $i . " месяц. Долг равен " . $debt . ". Выплачиваем \n";
+            echo " Выплата по долгу равна " . $spent . ". Выплачиваем 5000 \n";
+        } elseif ($debt <= 5000 and $debt >= 0) {
+            echo $i . " месяц. Долг равен " . $debt . ". Выплачиваем {$debt}. Поздравляем с последней выплатой. \n";
+            $spent = $spent + $debt;
+            $debt = $debt - $debt;
+            echo " Выплата по долгу равна " . $spent . ". Выплачиваем 5000 \n";
             break;
-        } elseif ($dolg < 0) {
-           // echo "Что-то пошло не так.";
+        } elseif ($debt < 0) {
+            // echo "Что-то пошло не так.";
             break;
         }
     }
-    //echo $potracheno . " потрачено<br>";
+    //echo $spent . " потрачено<br>";
     //echo $i . "месяцев<br>";
-    $potracheno=round($potracheno, 2);
-    return $potracheno;
+    $spent = round($spent, 2);
+    return $spent;
 }
-$homoCreditTotal =  kredit(39999, 4, 500, 0);
-$softBankTotal =  kredit(39999, 3, 1000, 0);
-$strawberryBankTotal =  kredit(39999, 2, 0, 7777);
+
+$homoCreditTotal = kredit(39999, 4, 500, 0);
+$softBankTotal = kredit(39999, 3, 1000, 0);
+$strawberryBankTotal = kredit(39999, 2, 0, 7777);
 echo "homoCredit: {$homoCreditTotal} руб. \n";
 echo "softBank: {$softBankTotal} руб. \n";
 echo "strawberryBank: {$strawberryBankTotal} руб. \n";
