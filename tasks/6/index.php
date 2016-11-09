32789505b20d964bcb8d16f8e366e8e368aabed0
Anton Vinogradov <etheralgreen@gmail.com>
2016-11-09 11:38:33 +1100

    6 task. Code optimizated

--- tasks/6/index.php
+++ tasks/6/index.php
@@ -1,28 +1,23 @@
 <?php
 header("Content-Type: text/plain; charset=utf-8");
-
 $credit = 40000;
 $percent = 3;
 $commission = 1000;
 $mounthlyPayment = 5000;
 $worthMoney = 0;
-
-for ($i = 1; $i < 1000; $i++) {
-    // К оплате кредита (К оплате за этот месяц + кредит)
+for ($i = 1; $i < 100; $i++) {
     $oversum = $credit + $commission + ($credit * ($percent / 100));
-    echo "В этом месяце ваш долг равен" . $oversum . "\n";
+    echo "В этом месяце ваш долг равен " . $oversum . "\n";
 
-    if ($oversum > 5000) {
+    if ($oversum > $mounthlyPayment) {
         $credit = $oversum - $mounthlyPayment;
-        $worthMoney = $worthMoney + $mounthlyPayment;
-        echo $i . " платёж. Выплачено " . $worthMoney . " рублей. Остаток по кредиту - " . $credit . "\n";
-        echo round($credit, 2) . "\n";
-    } elseif ($oversum <= 5000) {
+        $worthMoney += $mounthlyPayment;      // Убрал повторения
+    } else {
         $credit = 0;
-        $worthMoney = $worthMoney + $oversum;
-        echo "Последний " . $i . " платёж. Выплачено " . $worthMoney . " рублей. Остаток по кредиту - " . $credit . "\n";
+        $worthMoney += $oversum;             // Убрал повторения
         break;
     }
+    echo $i . " платёж. Выплачено " . $worthMoney . " рублей. Остаток по кредиту - " . $credit . "\n";
 }
 echo "Школьник избавится от кредита через " . $i . " месяцев \n";
-echo "Iphone стоил школьнику в " . round($worthMoney) . " рублей";
+echo "Iphone стоил школьнику в " . round($worthMoney, 2) . " рублей";  // Добавил копейки
